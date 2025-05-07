aa9c34db092a91f7a92c9927756ff19e7e5fad96
mohamed abass tawana <aminabass987@gmail.com>
2025-05-07 21:43:27 +0300

    nimeongeza

---
+++ data-types/dicts.py
@@ -0,0 +1,11 @@
+my_dict = {"name": "Mohamed", "age": 25, "city": "Cairo"}
+print(my_dict["city"])  
+
+my_dict={
+    "name": "Mohamed",
+    "age": 25,
+    "city": "Cairo",
+    "hobbies": ["reading", "coding", "gaming"],
+    "is_student": False,
+}
+print(my_dict["hobbies"][1])  
